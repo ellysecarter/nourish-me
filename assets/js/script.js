63524d82933eb5538f8b32d45d77f44eb8c0e34e
unknown <sean.hool@gmail.com>
2021-09-09 20:59:01 -0700

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//sets ElementById("sourceLink") to display and link to the recipe
 function setLink(id) {
     $.ajax({
         url:"https://api.spoonacular.com/recipes/"+id+"/information?apiKey=66f53e7e0ca942c9806998c27a0847af",
@@ -8,6 +9,7 @@ function setLink(id) {
     });
 }
 
+//sets ElementById("ingredients") to the recipe's ingredients seperated by <br>s
 function setIngredients(id) {
     $.ajax({
         url:"https://api.spoonacular.com/recipes/"+id+"/information?apiKey=66f53e7e0ca942c9806998c27a0847af",
@@ -22,11 +24,13 @@ function setIngredients(id) {
     });
 }
 
+//is called by the button to search for one recipe and set ElementById("title") to its title as well as call other functions
+//two commented out lines contain code for displaying the recipe's corresponding image and how long it takes to prepare
 function getRecipe(q) {
     $.ajax({
         url: "https://api.spoonacular.com/recipes/search?apiKey=66f53e7e0ca942c9806998c27a0847af&number=1&query="+q,
         success: function(res){
-            document.getElementById("output").innerHTML="<h1>"+res.results[0].title+"</h1><br>"+
+            document.getElementById("title").innerHTML="<h1>"+res.results[0].title+"</h1><br>"+
             //image "<img src='"+res.baseUri+res.results[0].image+"' width='400' /> <br>"+
             //"Ready in "+res.results[0].readyInMinutes+" minutes"
             setIngredients(res.results[0].id)
