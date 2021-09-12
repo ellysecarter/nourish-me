5e645bc2d8d2f4807570f6ec6cf10a7d90a4cb16
Wendy Nguyen <wendy.nguyen@gmail.com>
2021-09-12 05:55:10 -0700

    bug fixes

--- fcd-api/single.js
+++ fcd-api/single.js
@@ -1,6 +1,9 @@
 var nutritionContainerEl = document.querySelector("#nutrition-container");
 var descriptionEl = document.querySelector("#nutrient-description");
+var servingSizeEl = document.querySelector("#serving-size");
 var ingredientNameEl = document.querySelector("#ingredient-name");
+var searchFormEl = document.querySelector("#search-form");
+var ingredientInputEl = document.querySelector("#ingredient");
 
 function getIngredientName() {
     var queryString = document.location.search;
@@ -9,20 +12,20 @@ function getIngredientName() {
         ingredientNameEl.textContent = ingredientName.replace("%20", " ");
         getIngredientInfo(ingredientName);
     }else {
-        document.location.replace("./index.html");
+        searchFormEl.setAttribute("style", "display: flex");
       }
 }
 
 
 var getIngredientInfo = function(ingredient) {
     var apiUrl = "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=CaaqbdHoAR2KavNuAQFcBRRv7vL4gDF78ENsyxMu&query=" + ingredient + "&dataType=Survey (FNDDS)&pagesize=5";
-    console.log(apiUrl)
+    
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
             response.json().then(function(data) {
                 descriptionEl.textContent = data.foods[0].description;
-                console.log(data.foods[0].foodNutrients.length);
+                console.log(data);
                 console.log(data.foods[0].foodNutrients);
 
                 for (var i = 0; i < data.foods[0].foodNutrients.length; i++){ 
@@ -69,54 +72,69 @@ var getIngredientInfo = function(ingredient) {
       });
 };
   
-var displayIssues = function(issues) {
-    if (issues.length === 0) {
-        issueContainerEl.textContent = "This repo has no open issues!";
-        return;
-      }
+// var displayIssues = function(issues) {
+//     if (issues.length === 0) {
+//         issueContainerEl.textContent = "This repo has no open issues!";
+//         return;
+//       }
 
-    for (var i = 0; i < issues.length; i++) {
-        // create a link element to take users to the issue on github
-        var issueEl = document.createElement("a");
-        issueEl.classList = "list-item flex-row justify-space-between align-center";
-        issueEl.setAttribute("href", issues[i].html_url);
-        issueEl.setAttribute("target", "_blank");
+//     for (var i = 0; i < issues.length; i++) {
+//         // create a link element to take users to the issue on github
+//         var issueEl = document.createElement("a");
+//         issueEl.classList = "list-item flex-row justify-space-between align-center";
+//         issueEl.setAttribute("href", issues[i].html_url);
+//         issueEl.setAttribute("target", "_blank");
         
-        // create span to hold issue title
-        var titleEl = document.createElement("span");
-        titleEl.textContent = issues[i].title;
+//         // create span to hold issue title
+//         var titleEl = document.createElement("span");
+//         titleEl.textContent = issues[i].title;
 
-        // append to container
-        issueEl.appendChild(titleEl);
+//         // append to container
+//         issueEl.appendChild(titleEl);
 
-        // create a type element
-        var typeEl = document.createElement("span");
+//         // create a type element
+//         var typeEl = document.createElement("span");
 
-        // check if issue is an actual issue or a pull request
-        if (issues[i].pull_request) {
-        typeEl.textContent = "(Pull request)";
-        } else {
-        typeEl.textContent = "(Issue)";
-        }
+//         // check if issue is an actual issue or a pull request
+//         if (issues[i].pull_request) {
+//         typeEl.textContent = "(Pull request)";
+//         } else {
+//         typeEl.textContent = "(Issue)";
+//         }
 
-        // append to container
-        issueEl.appendChild(typeEl);
-        issueContainerEl.appendChild(issueEl);
-      }
-};
+//         // append to container
+//         issueEl.appendChild(typeEl);
+//         issueContainerEl.appendChild(issueEl);
+//       }
+// };
 
-var displayWarning = function(repo) {
-    // add text to warning container
-    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+// var displayWarning = function(repo) {
+//     // add text to warning container
+//     limitWarningEl.textContent = "To see more than 30 issues, visit ";
     
-    var linkEl = document.createElement("a");
-    linkEl.textContent = "See More Issues on GitHub.com";
-    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
-    linkEl.setAttribute("target", "_blank");
+//     var linkEl = document.createElement("a");
+//     linkEl.textContent = "See More Issues on GitHub.com";
+//     linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+//     linkEl.setAttribute("target", "_blank");
   
-    // append to warning container
-    limitWarningEl.appendChild(linkEl);
+//     // append to warning container
+//     limitWarningEl.appendChild(linkEl);
+// };
+
+var formSubmitHandler = function(event){
+    event.preventDefault();
+    // get value from input element
+    var ingredient = ingredientInputEl.value.trim();
+
+    if (ingredient) {
+        getIngredientInfo(ingredient);
+        ingredientInputEl.value = "";
+        } else {
+        alert("Please enter an ingredient");
+        }
+        
 };
 
 getIngredientName();
   
+searchFormEl.addEventListener("submit", formSubmitHandler);
